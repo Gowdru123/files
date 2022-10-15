bff31691b3764fd6b8221f5fb7d1b19821bf64b5
bavabee <93858110+bavabee@users.noreply.github.com>
2022-10-15 15:35:54 +0530

    Update start.py

--- plugins/start.py
+++ plugins/start.py
@@ -8,7 +8,7 @@ from pyrogram.types import (
 from pyrogram import filters
 
 PREFIX = ["/", ".", "?", "#", "@", "₹", "+", ":", "!", "^", "|"]
-
+START_MEDIA = "https://telegra.ph/file/d20dee1ba93fc0b0c05ac.jpg"
 START_TEXT = """I'ᴍ Uʟᴛʀᴀ Fᴀsᴛ Tᴇʟᴇɢʀᴀᴍ Cᴏᴜʟᴅ Sᴛᴏʀᴀɢᴇ Bᴏᴛ  Fᴏʀ [Tᴀᴍɪᴢʜᴀ Mᴀsᴛᴇʀ](http://tamizhmasters.com). Sᴇɴᴅ Mᴇ Aɴʏ Fɪʟᴇs/Lɪɴᴋs Aɴᴅ Sᴇʟᴇᴄᴛ Mᴇᴛʜᴏᴅ Wᴀɪᴛ Fᴇᴡ Sᴇᴄᴏɴᴅs Bᴏᴛ Wɪʟʟ Bᴇ Uᴘʟᴏᴀᴅ Tᴏ Oᴜʀ Sᴇʀᴠᴇʀ Aɴᴅ Gᴇɴᴀʀᴀᴛᴇ  [Tᴀᴍɪᴢʜᴀ Mᴀsᴛᴇʀ](http://tamizhmasters.com) ......
 
 Cᴜʀʀᴇɴᴛʟʏ Sᴜᴘᴘᴏʀᴛᴇᴅ Fᴏʀᴍᴀᴛs :
@@ -25,5 +25,5 @@ Pᴏᴡᴇʀᴇᴅ Bʏ : [Tᴀᴍɪᴢʜᴀ Mᴀsᴛᴇʀ](http://tamizhmasters.
 
 @bot.on_message(filters.command("start", PREFIX))
 async def help(client, message):
-    await message.reply_video(START_MEDIA, caption=START_TEXT,
+    await message.reply_photo(START_MEDIA, caption=START_TEXT,
                               reply_markup=InlineKeyboardMarkup(START_BUTTON))
