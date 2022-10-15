134255efc6fa72fc29c266048622e3c81c41f31e
bavabee <93858110+bavabee@users.noreply.github.com>
2022-10-15 21:19:01 +0530

    Update b.py

--- b.py
+++ b.py
@@ -5,6 +5,6 @@ from database.url_db import *
 from e import *
 
 DB_URI = os.environ.get("DB_URI")
-client = TelegramClient('ShortUrlLink', APP_ID, API_HASH).start(
+client = TelegramClient('ShortUrlLink', APP_ID, APP_HASH).start(
     bot_token=BOT_TOKEN)
 
