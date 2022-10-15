371e508caa658f0e76adebb8bdca68f167cb3d82
bavabee <93858110+bavabee@users.noreply.github.com>
2022-10-15 18:28:22 +0530

    Create data_shorten.py

---
+++ data_shorten.py
@@ -0,0 +1,26 @@
+from pymongo import MongoClient
+from configs import *
+
+myclient = MongoClient(DATABASE_URL)
+db = myclient['mydb']
+apiDB = db['api']
+
+
+async def info_taker(chat):
+    data = apiDB.find_one({"chat": chat})
+    if data is None:
+        return False, False
+    print(data)
+    api = data['api']
+    return api
+
+
+async def api_logger(chat, api):
+    data = apiDB.find_one({"chat": chat})
+
+    if data is None:
+        apiDB.insert_one({"chat": chat, "api": api})
+    else:
+        apiDB.delete_one(data)
+        apiDB.insert_one({"chat": chat, "api": api})
+
