2fb55d52e345853bc79759fbfd4d2a5e04a9dcb6
bavabee <93858110+bavabee@users.noreply.github.com>
2022-10-15 21:03:11 +0530

    Delete client.py

--- client.py
+++
@@ -1,9 +0,0 @@
-from c import *
-from f import *
-from data_shorten import *
-from shorten_link import *
-from link_shorten import *
-
-client = TelegramClient('ShortUrlLink', APP_ID, API_HASH).start(
-    bot_token=BOT_TOKEN)
-
