41b15397b878b7a87bf73fd51c221fb36f2857dd
bavabee <93858110+bavabee@users.noreply.github.com>
2022-10-15 17:17:44 +0530

    Update bot.py

--- bot.py
+++ bot.py
@@ -63,7 +63,7 @@ ABOUT_BOT_TEXT = f"""
 This is Permanent Files Store Bot!
 Send me any file I will save it in my Database. Also works for channel. Add me to channel as Admin with Edit Permission, I will add Save Uploaded File in Channel & add Sharable Button Link.
 
-🤖 **My Name:** [Files Store Bot](https://t.me/{BOT_USERNAME})
+🤖 **My Name:** [Tᴀᴍɪᴢʜᴀ Mᴀsᴛᴇʀ](https://t.me/tmfile_short_bot)
 
 📝 **Language:** [Python3](https://www.python.org)
 
