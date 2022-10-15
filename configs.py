cf9c3282a6b24aa0ec9a8a4be0cff6c6d1a8c773
bavabee <93858110+bavabee@users.noreply.github.com>
2022-10-15 13:47:21 +0530

    Copyed

--- configs.py
+++ configs.py
@@ -1,4 +1,4 @@
-# # (c) @HMF_Owmer_1
+# # (c) @AbirHasan2005
 
 import os
 
@@ -10,7 +10,7 @@ class Config(object):
 	BOT_USERNAME = os.environ.get("BOT_USERNAME")
 	DB_CHANNEL = int(os.environ.get("DB_CHANNEL", "-100"))
 	BOT_OWNER = int(os.environ.get("BOT_OWNER", "1445283714"))
-	DATABASE_URI = os.environ.get("DATABASE_URL")
+	DATABASE_URL = os.environ.get("DATABASE_URL")
 	UPDATES_CHANNEL = os.environ.get("UPDATES_CHANNEL", "")
 	LOG_CHANNEL = os.environ.get("LOG_CHANNEL", None)
 	BANNED_USERS = set(int(x) for x in os.environ.get("BANNED_USERS", "1234567890").split())
@@ -19,10 +19,7 @@ class Config(object):
 	BANNED_CHAT_IDS = list(set(int(x) for x in os.environ.get("BANNED_CHAT_IDS", "-1001362659779 -1001255795497").split()))
 	OTHER_USERS_CAN_SAVE_FILE = bool(os.environ.get("OTHER_USERS_CAN_SAVE_FILE", True))
 	BASE_SITE = os.environ.get("BASE_SITE", "tamizhmasters.com")
-	DOMAIN = os.environ.get("DOMAIN", "tamizhmasters.com")
-        ADMINS = os.environ.get("ADMINS")
-        PUBLIC_FILE_STORE = os.environ.get("PUBLIC_FILE_STORE")
-        FILE_STORE_CHANNEL = os.environ.get("FILE_STORE_CHANNEL")
+	DOMAIN = os.environ.get("DOMAIN", "urlsopen.com")
 	ABOUT_BOT_TEXT = f"""
 This is Permanent Files Store Bot!
 Send me any file I will save it in my Database. Also works for channel. Add me to channel as Admin with Edit Permission, I will add Save Uploaded File in Channel & add Sharable Button Link.
@@ -44,7 +41,7 @@ Send me any file I will save it in my Database. Also works for channel. Add me t
 	ABOUT_DEV_TEXT = f"""
 **🌐 This Bot Was Devloped By** : @A2z_tech"""
 	HOME_TEXT = """
-Hello [{}](tg://user?id={})✋\n\n☁️ This Is A Unlimited Telegram Could Storage Bot For shorturllink.in. Send Me Any File And Select Method Wait Few Seconds Bot Will Be Upload To Our Server And Genarate shorturllink.in Link For Files. ⚡\n\nCurrently Supported Format :\n\n⟴ File 📁\n⟴ Video 🎥\n⟴ Photo 🖼️\n⟴ Audio 🎙️\n\nMore Format Soon ⚡\n\nNote : 𝗬𝗢𝗨 𝗖𝗔𝗡 𝗔𝗟𝗦𝗢 𝗨𝗣𝗟𝗢𝗔𝗗 𝟰𝗚𝗕 𝗙𝗜𝗟𝗘𝗦 📥"""
+Hello [{}](tg://user?id={})✋\n\n☁️ This Is A Unlimited Telegram Could Storage Bot For tamizhmasters.com Send Me Any File And Select Method Wait Few Seconds Bot Will Be Upload To Our Server And Genarate tamizhmasters.com Link For Files. ⚡\n\nCurrently Supported Format :\n\n⟴ File 📁\n⟴ Video 🎥\n⟴ Photo 🖼️\n⟴ Audio 🎙️\n\nMore Format Soon ⚡\n\nNote : 𝗬𝗢𝗨 𝗖𝗔𝗡 𝗔𝗟𝗦𝗢 𝗨𝗣𝗟𝗢𝗔𝗗 𝟰𝗚𝗕 𝗙𝗜𝗟𝗘𝗦 📥"""
 	SHORTENER_API_MESSAGE = """To add or update your Shortner Website API, `/apikey api`
             
 Ex: `/apikey 6LZq851sXofffPHugiKQq`
@@ -52,5 +49,4 @@ Ex: `/apikey 6LZq851sXofffPHugiKQq`
 Current Website: {base_site}
 
 Current Shortener API: `{shortener_api}`"""
-
 
