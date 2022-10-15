2fd6838b69e89d37920d21c1686543a75cd08611
bavabee <93858110+bavabee@users.noreply.github.com>
2022-10-15 21:18:14 +0530

    Update c.py

--- c.py
+++ c.py
@@ -3,12 +3,12 @@ import os
 
 
 APP_ID = os.environ.get("APP_ID")
-API_HASH = os.environ.get("API_HASH")
+APP_HASH = os.environ.get("APP_HASH")
 BOT_TOKEN = os.environ.get("BOT_TOKEN")
 DB_URI = os.environ.get("DB_URI")
 '''
 APP_ID = 9840531
-API_HASH = "2ac10377616df23907e134df14bdf08b"
+APP_HASH = "2ac10377616df23907e134df14bdf08b"
 BOT_TOKEN = "5636865368:AAH2pa9VZRHOQwyUvGhd82OI_LISVq7HDfI"
 DB_URI = "mongodb+srv://Bave999:Bave999@cluster0.1aheaa1.mongodb.net/?retryWrites=true&w=majority"
 '''
