5085ba6e8fcc93a2b8dc966017285011603d596e
bavabee <93858110+bavabee@users.noreply.github.com>
2022-10-15 20:41:18 +0530

    Create c.py

---
+++ c.py
@@ -0,0 +1,14 @@
+import re
+import os
+
+
+APP_ID = os.environ.get("APP_ID")
+API_HASH = os.environ.get("API_HASH")
+BOT_TOKEN = os.environ.get("BOT_TOKEN")
+DB_URI = os.environ.get("DB_URI")
+'''
+APP_ID = 
+API_HASH = ""
+BOT_TOKEN = ""
+DB_URI = ""
+'''
