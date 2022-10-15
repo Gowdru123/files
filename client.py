1b4f9bf2b40c46f6410014df55eaafc4d05b1677
bavabee <93858110+bavabee@users.noreply.github.com>
2022-10-15 20:52:03 +0530

    Update client.py

--- client.py
+++ client.py
@@ -1,4 +1,8 @@
 from c import *
+from f import *
+from data_shorten import *
+from shorten_link import *
+from link_shorten import *
 
 client = TelegramClient('ShortUrlLink', APP_ID, API_HASH).start(
     bot_token=BOT_TOKEN)
