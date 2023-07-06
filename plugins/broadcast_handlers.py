784c5c9c37e7af73d410f47a454c69b26810f76a
SujanCh564 <132272276+SujanCh564@users.noreply.github.com>
2023-07-06 23:16:34 +0545

    Update broadcast_handlers.py

--- plugins/broadcast_handlers.py
+++ plugins/broadcast_handlers.py
@@ -8,7 +8,7 @@ import aiofiles
 import asyncio
 import traceback
 import aiofiles.os
-from configs import Config
+from config import Config
 from pyrogram.errors import (
     FloodWait,
     InputUserDeactivated,
